d1f80974af5879cd9964b8212d776b8c68074836
AndranikAdyan <andranikadyan@gmail.com>
2024-10-30 19:17:16 +0400

    push bonus

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:19:52 by aadyan            #+#    #+#             */
-/*   Updated: 2024/10/29 16:14:21 by aadyan           ###   ########.fr       */
+/*   Updated: 2024/10/30 18:52:02 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,20 +64,18 @@ char	*ft_remainder(char *str)
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
+	static char	*str[66000]; // replace to OPEN_MAX
 	char		*s;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	read_line(fd, &str);
-	if (!str)
-		return (0);
-	s = ft_cutstr(str);
+	read_line(fd, &str[fd]);
+	s = ft_cutstr(str[fd]);
 	if (s && !*s)
 	{
 		free(s);
 		return (NULL);
 	}
-	str = ft_remainder(str);
+	str[fd] = ft_remainder(str[fd]);
 	return (s);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/10 13:13:20 by andranik          #+#    #+#             */
-/*   Updated: 2024/10/29 16:14:28 by aadyan           ###   ########.fr       */
+/*   Created: 2024/02/10 13:13:20 by aadyan            #+#    #+#             */
+/*   Updated: 2024/10/30 19:01:14 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@
 
 # include <fcntl.h>
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <stdio.h>
+# include <limits.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(char *str, char sign);
